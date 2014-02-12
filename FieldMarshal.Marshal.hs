d73ab83256bda534a2d6fc1003836e3c57b63a8a
Scott Fleischman <scott.fleischman@logos.com>
2014-02-12 01:09:38 -0800

    Add UnmanagedType

--- FieldMarshal.Marshal.hs
+++ FieldMarshal.Marshal.hs
@@ -1,18 +1,21 @@
 module FieldMarshal.Marshal where
-import FieldMarshal.CSharp as CSharp
-import FieldMarshal.Cpp as Cpp
+import qualified FieldMarshal.CSharp as CSharp
+import qualified FieldMarshal.Cpp as Cpp
 
 data MarshalType =
     MarshalDefault Cpp.Type CSharp.Type
-  | MarshalAs Cpp.Type CSharp.Type String
+  | MarshalAs Cpp.Type CSharp.Type UnmanagedType
   | MarshalWrap Cpp.Type CSharp.Type [CppTypeWrapper]
   | MarshalPlatform Cpp.Type CSharp.Type [(Platform, CppTypeWrapper)]
+  deriving (Eq, Show)
+
+newtype UnmanagedType = UnmanagedType String
   deriving (Eq, Show)
 
 data Platform = Microsoft | Mono
   deriving (Eq, Show)
 
-data ConversionCode = ConversionCode Cpp.Type [String] [String]
+data ConversionCode = ConversionCode UnmanagedType Cpp.Type [String] [String]
   deriving (Eq, Show)
 
 data CppTypeWrapper =
